f21497969efe3c73742b43a9d60776b63b1fdf30
lomiland <161277582+lomiland@users.noreply.github.com>
2024-04-08 12:50:34 +0800

    Update main.cpp

--- Qt/main.cpp
+++ Qt/main.cpp
@@ -1,11 +1,22 @@
-#include "widget.h"
-
+#include "mainwindow.h"
 #include <QApplication>
+#include <QLocale>
+#include <QTranslator>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    Widget w;
+
+    QTranslator translator;
+    const QStringList uiLanguages = QLocale::system().uiLanguages();
+    for (const QString &locale : uiLanguages) {
+        const QString baseName = "BIGhomework_" + QLocale(locale).name();
+        if (translator.load(":/i18n/" + baseName)) {
+            a.installTranslator(&translator);
+            break;
+        }
+    }
+    MainWindow w;
     w.show();
     return a.exec();
 }
